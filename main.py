2b6cbdf36bcdbe02e182fcdf2a02cdd562339cd7
Gurur Uğur <gurur.ugur@std.bogazici.edu.tr>
2025-03-07 16:48:07 +0300

    Implemented int_to_roman function

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
+    roman_values = {
+        1: "I", 4: "IV", 5: "V", 9: "IX",
+        10: "X", 40: "XL", 50: "L", 90: "XC",
+        100: "C", 400: "CD", 500: "D", 900: "CM",
+        1000: "M"
+    }
+    
+    # Sorting keys by descending order
+    sorted_keys = sorted(roman_values.keys(), reverse=True)
+    
+    roman_numeral = ""  # Our result string
 
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+    # Here I convert the number from maximum to minimum
+    for value in sorted_keys:
+        while num >= value:
+            roman_numeral += roman_values[value]  # Add the Roman symbol
+            num -= value  # Substract the number
+    
+    return roman_numeral
